3dde941ae096f094c56453fd5d41b62422c8e422
plgonzalezrx8 <plgonzalez@me.com>
2017-12-24 14:40:30 +0000

    Bug Fixes

--- balance.py
+++ balance.py
@@ -1,27 +1,28 @@
+#!/usr/bin/python2.7
 # -*- coding: UTF-8 -*-
 # @yasinkuyu
 
 from BinanceAPI import *
 
 import config
- 
+
 class Binance:
 
     def __init__(self):
         self.client = BinanceAPI(config.api_key, config.api_secret)
-    
+
     def balances(self):
         balances = self.client.get_account()
-    
+
         for balance in balances['balances'] :
-            if int(balance["locked"]) > 0 or int(balance["free"]) > 0 :
+            if float(balance["locked"]) > 0 or float(balance["free"]) > 0 :
                 print '%s: %s' % (balance['asset'], balance['free'])
 
     def orders(self, symbol, limit):
-        orders = self.client.get_all_orders(symbol=symbol, limit=limit)
+        orders = self.client.get_open_orders(symbol, limit)
 
-        print orders 
-    
+        print orders
+
     def tickers(self):
        return self.client.get_all_tickers()
 
@@ -30,15 +31,15 @@ class Binance:
 
     def openorders(self):
        return self.client.get_open_orders()
- 
+
 try:
-    
+
     m = Binance()
     #m.balances()
-    m.orders('WTCBTC',10)
+    m.orders('XVGBTC',10)
 
-except BinanceAPIException as e:
+except "BinanceAPIException" as e:
     print e.status_code
     print e.message
-     
+
 
