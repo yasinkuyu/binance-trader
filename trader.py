5e6aa5b2f4f89c26eaa8abcba1518ea7e7b5e2e8
WSC <35452719+WeSpeakCrypto@users.noreply.github.com>
2018-01-19 15:25:10 +0800

    Release for Quicktest (test commit)
    As mentioned here: https://github.com/yasinkuyu/binance-trader/issues/109
    Changes have been included and now open for testing / use.
    ***
    I tested this using BTC pair only and Profit mode.
    Please try using small amounts first.
    Most of the bugs are eliminated and others (if any) will be exposed if you help test this.
    Ofcourse while I'm doing trial and error. It put me to some loss (using very small profit % to trigger faster while testing)
    So if you like my changes and gain some. Donations are very welcome. LOL. (will be used for deeper testing before giving out the code)
    
    My Wallets:
    BTC wallet: 182Ew6JK9Mspw4BszdBP7RgpdWf6STe46G
    LTC wallet: LXgNmMPied4AiGKAsE1kY2M9BRaV3yxDC1
    ETH wallet: 0xed8100b70e15d9fcd53f1d989c67775bf55e4475
    
    Yasin Kuyu Wallets:
    http://yasinkuyu.net/wallet
    
    If you like the defaults (check it inside trader.py)
    - You can now start the program with just --symbol XXXBTC etc. as the only argument.
    
    Remember to read first my changes here: https://github.com/yasinkuyu/binance-trader/issues/109
    We can also use that thread for questions.
    
    Happy Hunting! :)

--- trader.py
+++ trader.py
@@ -12,20 +12,21 @@ if __name__ == '__main__':
     
     # Set parser
     parser = argparse.ArgumentParser()
-    parser.add_argument('--quantity', type=float, help='Buy/Sell Quantity', default=200)
+    parser.add_argument('--quantity', type=float, help='Buy/Sell Quantity', default=0)
     parser.add_argument('--symbol', type=str, help='Market Symbol (Ex: XVGBTC)', required=True)
-    parser.add_argument('--profit', type=float, help='Target Profit', default=1.3)
+    parser.add_argument('--profit', type=float, help='Target Profit', default=3)
+    parser.add_argument('--amount', type=float, help='Buy/Sell Amount', default=0.0022)
 
     # Stop-Loss (Todo: Not complated)
-    parser.add_argument('--stoploss', type=float, help='Target Stop-Loss % (If the price drops by 6%, sell market_price.)', default=0) 
+    parser.add_argument('--stoploss', type=float, help='Target Stop-Loss % (If the price drops by 6%, sell market_price.)', default=1) 
 
-    parser.add_argument('--increasing', type=float, help='Buy Price +Increasing (0.00000001)', default=0.00000001)
-    parser.add_argument('--decreasing', type=float, help='Sell Price -Decreasing (0.00000001)', default=0.00000001)
+    parser.add_argument('--increasing', type=float, help='Buy Price +Increasing (0.01)', default=0.2)
+    parser.add_argument('--decreasing', type=float, help='Sell Price -Decreasing (0.01)', default=0.2)
 
     # Manually defined --orderid try to sell 
     parser.add_argument('--orderid', type=int, help='Target Order Id (use balance.py)', default=0)
 
-    parser.add_argument('--wait_time', type=int, help='Wait Time (seconds)', default=1)
+    parser.add_argument('--wait_time', type=float, help='Wait Time (seconds)', default=0.7)
     parser.add_argument('--test_mode', type=bool, help='Test Mode True/False', default=False)
     parser.add_argument('--prints', type=bool, help='Scanning Profit Screen Print True/False', default=True)
     parser.add_argument('--debug', type=bool, help='Debug True/False', default=True)
