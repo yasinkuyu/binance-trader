12a16529eb5157c39ccc547f6632d8de5f8c39b8
Marcos Garcia <marcgarcia@groupon.com>
2018-01-21 11:17:08 +0000

    chore: log spread on screen log

--- app/Trading.py
+++ app/Trading.py
@@ -331,6 +331,8 @@ class Trading():
     
         # Screen log
         if self.option.prints and self.order_id == 0:
+            spreadPerc = (lastAsk/lastBid - 1) * 100.0
+            print ('price:%.8f buyp:%.8f sellp:%.8f-bid:%.8f ask:%.8f spread:%.2f' % (lastPrice, buyPrice, profitableSellingPrice, lastBid, lastAsk, spreadPerc))
             print ('price:%.8f buyp:%.8f sellp:%.8f-bid:%.8f ask:%.8f' % (lastPrice, buyPrice, profitableSellingPrice, lastBid, lastAsk))
         
         # analyze = threading.Thread(target=analyze, args=(symbol,))
